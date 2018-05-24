af6e210f0a9496756e01f1b3a08fcff56a43dbcb
Geovana Sembraski <geovanasembraski@MacBook-Pro-de-Geovana.local>
2018-05-24 19:39:47 -0300

    - Ajuste imagem da roleta. - Ajuste classe css roulette. - Ajuste delay splash (teste).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,12 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+
+      this.platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        splashScreen.hide();
+      });
     });
   }
 
